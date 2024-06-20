5f5930549723a160a91094630ba088f5b69130cf
xyaman <dbarriapaiva@gmail.com>
2024-06-20 10:08:34 +0900

    update harpoon

--- nvim/.config/nvim/lua/xyaman/lazy.lua
+++ nvim/.config/nvim/lua/xyaman/lazy.lua
@@ -48,7 +48,7 @@ local harpoon = require("harpoon")
 harpoon:setup()
 
 vim.keymap.set("n", "<leader>ss", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<leader>sa", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>sj", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>sk", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>sl", function() harpoon:list():select(3) end)
