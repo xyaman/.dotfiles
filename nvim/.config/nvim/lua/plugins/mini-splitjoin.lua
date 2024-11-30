4d79888ed3ebd46327a9a2af64f5de7ba3a6ec9a
xyaman <dbarriapaiva@gmail.com>
2024-11-30 17:38:20 +0900

    nvim: rename & treesj -> mini.splitjoin

---
+++ nvim/.config/nvim/lua/plugins/mini-splitjoin.lua
@@ -0,0 +1,9 @@
+-- Split/join blocks of code.
+return {
+    {
+        "echasnovski/mini.splitjoin",
+        config = function()
+            require('mini.splitjoin').setup()
+        end
+    },
+}
